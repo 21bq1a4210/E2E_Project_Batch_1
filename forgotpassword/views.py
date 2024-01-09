a6e18d190a8782ad9e3a6e0b2f124f833bcbdc17
Bandreddi.SarathChandra <21bq1a4210@vvit.net>
2024-01-09 20:37:40 +0530

    modified forgotpassword app

--- forgotpassword/views.py
+++ forgotpassword/views.py
@@ -4,30 +4,28 @@ from django.contrib import messages
 from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.forms import SetPasswordForm
 
-# Create your views here.
 def forgotpassword(request):
     if request.method == 'POST':
         email = request.POST.get('email')
         user = User.objects.filter(email=email).first()
-        print(email, user)
+        print(email,user)
         if user:
-            # Simulate OTP generation and verification
             generated_otp = "123456"  # Replace this with your actual OTP generation logic
             entered_otp = request.POST.get('otp')
 
             if entered_otp == generated_otp:
-                # OTP is verified, update the template context
                 if request.method == 'POST':
                     form = SetPasswordForm(user, request.POST)
+
                     if form.is_valid():
                         form.save()
                         update_session_auth_hash(request, form.user)
                         messages.success(request, 'Password updated successfully.')
-                        return redirect('password_reset_done')  # Redirect to a success page
+                        return redirect('password_reset_done')
                     else:
                         messages.error(request, 'Invalid form submission. Please try again.')
 
-                return render(request, 'forgotpassword.html', {'email_exists': True, 'otp_verified': True, 'form': form})
+                return render(request, 'forgotpassword.html', {'step': 3, 'email_exists': True, 'otp_verified': True, 'form': form})
 
             else:
                 messages.error(request, 'Invalid OTP. Please try again.')
@@ -35,4 +33,5 @@ def forgotpassword(request):
         else:
             messages.error(request, 'Email not found in the database.')
 
-    return render(request, 'forgotpassword.html', {'email_exists': False, 'otp_verified': False})
+    # Default to Step 1
+    return render(request, 'forgotpassword.html', {'step': 1, 'email_exists': False, 'otp_verified': False})
